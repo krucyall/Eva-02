514d822e4700b3167c5d4096d8df05ca47acb858
NetAcad DEVASC <changeme@example.com>
2026-05-29 20:10:49 +0000

    Script definitivo corregido

--- sample-app.sh
+++ sample-app.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-
+rm -rf tempdir
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
-
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "FROM python" > tempdir/Dockerfile
+echo "RUN pip install --progress-bar off flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-docker run -t -d -p 9999:8080 --name sampleapp
-docker ps -a 
+docker run -t -d -p 9999:8080 --name samplerunning sampleapp
